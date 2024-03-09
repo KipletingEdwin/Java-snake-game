f0e7fe08d6e1c1cb06af8e5dadace58842394556
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-09 22:31:10 +0300

    add methods needed for the program

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,46 @@
+
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     GamePanel(){
 
     }
+    public void  startGame(){
+
+    }
+    public  void paintComponent(){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public  void keyPressed(KeyEvent e){
+
+        }
     }
 }
